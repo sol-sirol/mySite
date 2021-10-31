1af6fc6e24604061202f8cbd280acc6004c02760
sol-sirol <arturkamertsel01@icloud.com>
2021-10-31 16:07:26 +0300

    Добавил скрол плагином SimpleBar

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ function scripts() {
     return src([
         // 'node_modules/jquery/dist/jquery.js',
         // 'node_modules/swiper/swiper-bundle.min.js',
+        'node_modules/simplebar/dist/simplebar.min.js',
         'app/js/main.js'
     ])
     .pipe(concat("main.min.js"))
