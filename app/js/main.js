0efae6bc03fff5a710c77685a61db26abd4c0d1a
sol-sirol <arturkamertsel01@icloud.com>
2021-10-31 13:51:50 +0300

    Готова логика работы меню

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+const mainContent = document.querySelector(".wrapper__main-content");
+const buttonClous = document.querySelector(".button-clous");
+const buttonClousLink = document.querySelector(".button-clous-link");
+const buttonOpen = document.querySelector(".button-open");
+
+buttonClous.addEventListener("click", () => {
+  mainContent.classList.remove("_active")
+})
+buttonClousLink.addEventListener("click", () => {
+  mainContent.classList.remove("_active");
+  mainContent.classList.add("_active-following");
+})
+buttonOpen.addEventListener("click", () => {
+  mainContent.classList.add("_active")
+})
