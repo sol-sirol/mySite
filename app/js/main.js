316bb0d89d992159d20fa8d451600642818e9482
sol-sirol <arturkamertsel01@icloud.com>
2022-01-14 20:25:14 +0300

    Адаптировал блок philosophy

--- app/js/main.js
+++ app/js/main.js
@@ -3,16 +3,18 @@ const buttonClous = document.querySelector(".button-clous");
 const buttonClousLink = document.querySelector(".button-clous-link");
 const buttonOpen = document.querySelector(".button-open");
 
+// меню =======================
 buttonClous.addEventListener("click", () => {
-  mainContent.classList.remove("_active")
-})
+  mainContent.classList.remove("_active");
+});
 buttonClousLink.addEventListener("click", (e) => {
   //e.preventDefault();
   //setTimeout(() => window.location.replace(e.target.href), 500);
-  
+
   mainContent.classList.remove("_active");
   mainContent.classList.add("_active-following");
-})
+});
 buttonOpen.addEventListener("click", () => {
-  mainContent.classList.add("_active")
-})
+  mainContent.classList.add("_active");
+});
+// меню =======================
